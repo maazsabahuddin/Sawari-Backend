289d426385b4ab16848d27c6f28de465c3a5406c
Sohaib Aijaz <sohaibaijaz9@gmail.com>
2019-12-01 22:25:26 +0500

    Add models in Reservation Model

--- Reservation/models.py
+++ Reservation/models.py
@@ -5,16 +5,26 @@ from User.models import Captain, Customer
 
 
 class Vehicle(models.Model):
-    vehicle_no_plate = models.CharField(blank=False, null=False, max_length=10, unique=True)
+    vehicle_no_plate = models.CharField(blank=False, max_length=10)
     driver_ids = models.ManyToManyField(Captain, related_name='drivers', blank=True)
     owner = models.ForeignKey(Captain, on_delete=models.CASCADE)
     brand = models.CharField(blank=True, max_length=20)
     max_seats = models.IntegerField(blank=False)
-    from_loc = models.CharField(max_length=255)
-    to_loc = models.CharField(max_length=255)
+    from_loc = models.CharField(max_length=255, default="K")
+    to_loc = models.CharField(max_length=255, default="L")
 
     def __str__(self):
         return "Vehicle {} - {}".format(self.id, self.vehicle_no_plate)
+
+
+class Route(models.Model):
+    start_lat_long = models.CharField(blank=False, max_length=100)
+    start_name = models.CharField(blank=False, max_length=50)
+    stop_lat_long = models.CharField(blank=False, max_length=100)
+    stop_name = models.CharField(blank=False, max_length=50)
+
+    def __str__(self):
+        return "{} - {}".format(self.start_name, self.stop_name)
 
 
 class Ride(models.Model):
@@ -22,7 +32,7 @@ class Ride(models.Model):
     vehicle_id = models.ForeignKey(to='Vehicle', on_delete=models.CASCADE, related_name='driver_vehicle')
     start_time = models.DateTimeField(blank=True, null=True)
     end_time = models.DateTimeField(blank=True, null=True)
-    route = models.CharField(blank=True, max_length=256)
+    route_id = models.ManyToManyField(Route, related_name='ride_route')
     seats_left = models.IntegerField(blank=False, null=False)
 
     def __str__(self):
@@ -41,3 +51,11 @@ class Reservation(models.Model):
     def __str__(self):
         return "{} - {} - {}".format(self.id, self.customer_id.user.email, self.customer_id.user.phone_number)
 
+
+class Stop(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    lat_long = models.CharField(blank=False, max_length=100)
+    route_id = models.ManyToManyField(Route, related_name='route_stops')
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.name, self.lat_long, self.route_id)
